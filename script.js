c01ca35667b64ed4572a3616ff99de03bb3fcd80
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2021-03-03 20:56:32 +0100

    Check for errors and negative numbers, add loading animation

--- script.js
+++ script.js
@@ -2,32 +2,59 @@ const currencyEl_one = document.getElementById("currency-one");
 const amountEl_one = document.getElementById("amount-one");
 const currencyEl_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
+const loader = document.querySelector("#loading");
 
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
+// showing loading
+const displayLoading = () => {
+  loader.classList.add("display");
+  // to stop loading after some time
+  setTimeout(() => {
+    loader.classList.remove("display");
+  }, 5000);
+};
+
+// hiding loading
+const hideLoading = () => {
+  loader.classList.remove("display");
+};
+
 // Fetch exchange rates and update the DOM
 async function calculate() {
+  displayLoading();
   const currency_one = currencyEl_one.value;
-  let response = await fetch(
-    `https://api.exchangerate-api.com/v4/latest/${currency_one}`
-  );
-  let data = await response.json().then((data) => {
-    const currency_two = currencyEl_two.value;
+  try {
+    let response = await fetch(
+      `https://api.exchangerate-api.com/v4/latest/${currency_one}`
+    );
+    let data = await response.json().then((data) => {
+      hideLoading();
+      const currency_two = currencyEl_two.value;
 
-    const rate = data.rates[currency_two];
+      const rate = data.rates[currency_two];
 
-    rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-    amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-  });
-
-  return data;
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+    return data;
+  } catch (error) {
+    alert(error);
+  }
 }
 
 // Event listeners
 currencyEl_one.addEventListener("change", calculate);
-amountEl_one.addEventListener("input", calculate);
+amountEl_one.addEventListener("input", () => {
+  // Check for negative numbers
+  if (amountEl_one.value <= 0) {
+    alert("Must be a number larger than 0");
+  } else {
+    calculate();
+  }
+});
 currencyEl_two.addEventListener("change", calculate);
 amountEl_two.addEventListener("input", calculate);
 
